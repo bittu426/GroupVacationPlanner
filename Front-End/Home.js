b75602f9fee58ba25b516fa2cebedffab9a5bd3f
rle115 <84989072+rle115@users.noreply.github.com>
2022-04-08 21:21:06 -0400

    Add files via upload
    Basic homepage and menu bar for the site. Filler text with no buttons or functionality

---
+++ Front-End/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+  static displayName = Home.name;
+
+  render () {
+    return (
+      <div>
+        <h1>Vacation Planner</h1>
+        <p>Welcome to the Vacation planner!</p>
+        <ul>
+         
+        </ul>
+        <p>Filler Text Here</p>
+      </div>
+    );
+  }
+}
