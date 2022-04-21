ad467463f119e82d90e2186a89909813c44eb06a
bittu <chaitanya.bittu426@gmail.com>
2022-04-21 19:56:53 -0400

    Checking

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -2,40 +2,75 @@ import React, { useState, useEffect } from "react";
 import Home from "./Home";
 import { Component } from "react";
 
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Register from "./Register";
 import Login from "./Login";
-import APIService from './APIService'
-import useToken from './useToken'
-import Profile from './Profile'
+import APIService from "./APIService";
+import useToken from "./useToken";
+import Profile from "./Profile";
+import Groups from "./Groups";
 
-import Message from './Message'
+import Message from "./Message";
 import Modal from "./messagepage";
 import Header from "./Header";
 import Plan from "./Plan";
-const apiservice = new APIService() ;
-
-
+const apiservice = new APIService();
 
 function App() {
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const { token, removeToken, setToken } = useToken();
-    return (
-      <div id="App">
-        <Routes>
-          <Route path="/login" element={<Login apiservice={apiservice} setUserName={setUserName} setPassword={setPassword} />} />
-          <Route path="/" element={<Header apiservice={apiservice} setUserName={setUserName} setPassword={setPassword} /> } />
-          <Route path="/register" element={<Register apiservice={apiservice} />} />
-          <Route path="/home" element={<Home  />} />
-          <Route path="/plan" element={<Plan />} />
-          <Route path="/plan" render={() => {window.location.href="/Users/asearles/Documents/Github/GroupVacationPlanner/frontend/src/Ka.html"}} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/messages" element={<Message apiservice={apiservice} username={username} password={password} />} />
-        </Routes>
-     {/*}   <NavMenu token={removeToken}/>
+  return (
+    <div id="App">
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            <Login
+              apiservice={apiservice}
+              setUserName={setUserName}
+              setPassword={setPassword}
+            />
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <Header
+              apiservice={apiservice}
+              setUserName={setUserName}
+              setPassword={setPassword}
+            />
+          }
+        />
+        <Route
+          path="/register"
+          element={<Register apiservice={apiservice} />}
+        />
+        <Route path="/home" element={<Home />} />
+        <Route path="/plan" element={<Plan />} />
+        <Route
+          path="/plan"
+          render={() => {
+            window.location.href =
+              "/Users/asearles/Documents/Github/GroupVacationPlanner/frontend/src/Ka.html";
+          }}
+        />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/groups" element={<Groups />} />
+        <Route
+          path="/messages"
+          element={
+            <Message
+              apiservice={apiservice}
+              username={username}
+              password={password}
+            />
+          }
+        />
+      </Routes>
+      {/*}   <NavMenu token={removeToken}/>
         {!token && token!=="" &&token!== undefined?  
         <Login setToken={setToken} />
         :(
@@ -54,10 +89,7 @@ function App() {
             
         </Routes>
         */}
-          
-          
-      </div>
-    );
-  
+    </div>
+  );
 }
 export default App;
---
+++ frontend/src/components/Groups.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header";
+
+import "../styles/Groups.css";
+
+function Groups() {
+  return (
+    <div>
+      <Header />
+      <div className="groups-container">
+        <h1>Groups page</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Groups;
